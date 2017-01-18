bfe9b739faa430624d6d3de0565fdd855a120141
Valeryi Savich <Relrin78@gmail.com>
2017-01-18 23:30:16 +0300

    Displaying gold sprite depends from the its amount

--- game/src/Gold.cpp
+++ game/src/Gold.cpp
@@ -5,12 +5,22 @@
 Gold::Gold()
 {
 	// Set gold value.
-	this->goldValue = 15;
+	this->goldValue = std::rand() % 21 + 5;
 
 	// Set the sprite.
 	int textureID;
-	textureID = TextureManager::AddTexture("../resources/loot/gold/spr_pickup_gold_medium.png");
+	if (this->goldValue < 9)
+	{
+		textureID = TextureManager::AddTexture("../resources/loot/gold/spr_pickup_gold_small.png");
+	}
+	else if (this->goldValue >= 16)
+	{
+		textureID = TextureManager::AddTexture("../resources/loot/gold/spr_pickup_gold_large.png");
+	} else {
+		textureID = TextureManager::AddTexture("../resources/loot/gold/spr_pickup_gold_medium.png");
+	}
 
+	// Set the sprite.
 	this->SetSprite(TextureManager::GetTexture(textureID), false, 8, 12);
 
 	// Set the item type.
